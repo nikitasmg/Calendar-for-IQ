0ec9dc0056205bdb04853e5fb53236a9eeccef76
Nikita <nvsmagin@mail.ru>
2021-11-10 04:22:47 +0700

    Added popup functions, added days attributs, fixed some bugs

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const date = new Date();
 
- 
-const renderCalendar = ()=> {
+
+const renderCalendar = () => {
     date.setDate(1);
-    
-    const lastDay = new Date(date.getUTCFullYear(), date.getMonth() + 1,0).getDate();
+
+    const lastDay = new Date(date.getUTCFullYear(), date.getMonth() + 1, 0).getDate();
     const firstDayIndex = date.getDay() - 1;
-    const lastDayIndex = new Date(date.getUTCFullYear(), date.getMonth() + 1,0).getDay();
+    const lastDayIndex = new Date(date.getUTCFullYear(), date.getMonth() + 1, 0).getDay();
     const nextDays = 7 - lastDayIndex;
-    const lastPrevDay = new Date(date.getUTCFullYear(), date.getMonth(),0).getDate();
+    const lastPrevDay = new Date(date.getUTCFullYear(), date.getMonth(), 0).getDate();
 
     let template = document.querySelector('.calendar__template');
     let mounths = [
@@ -25,6 +25,20 @@ const renderCalendar = ()=> {
         'Ноябрь',
         'Декабрь'
     ]
+    let mounthsAtribute = [
+        'января',
+        'февраля',
+        'марта',
+        'апреля',
+        'мая',
+        'июня',
+        'июля',
+        'августа',
+        'сентября',
+        'октября',
+        'ноября',
+        'декабря'
+    ]
 
     let week = [
         'Понедельник',
@@ -40,70 +54,47 @@ const renderCalendar = ()=> {
 
     let days = '';
 
-    
-    if (firstDayIndex >=  0) {
-        for(let x = firstDayIndex; x > 0; x--) {
-            days += `<div class="calendar__cell"></span> <span class="day">${lastPrevDay - x + 1}</span></div>`
-        }
-    }else {
-        for(let x = 6; x > 0; x--) {
-            days += `<div class="calendar__cell"></span> <span class="day">${lastPrevDay - x + 1}</span></div>`
-        }
-    }    
 
-    for(let i = 1; i <= lastDay; i++) {
-        days += `<div class="calendar__cell"></span> <span class="day">${i}</span></div>`;
+    if (firstDayIndex >= 0) {
+        for (let x = firstDayIndex; x > 0; x--) {
+            days += `<div class="calendar__cell" day="${lastPrevDay - x + 1} ${mounthsAtribute[date.getMonth()-1]}"></span> <span class="day">${lastPrevDay - x + 1}</span></div>`
+        }
+    } else {
+        for (let x = 6; x > 0; x--) {
+            days += `<div class="calendar__cell" day="${lastPrevDay - x + 1} ${mounthsAtribute[date.getMonth()-1]}"></span> <span class="day">${lastPrevDay - x + 1}</span></div>`
+        }
     }
 
-    for(j = 1; j <= nextDays; j++) {
-        if(nextDays !== 7) {
-        days += `<div class="calendar__cell"></span> <span class="day">${j}</span></div>`;
-        }   
+    for (let i = 1; i <= lastDay; i++) {
+        days += `<div class="calendar__cell" day="${i} ${mounthsAtribute[date.getMonth()]}"></span> <span class="day">${i}</span></div>`;
+    }
+
+    for (j = 1; j <= nextDays; j++) {
+        if (nextDays !== 7) {
+            days += `<div class="calendar__cell" day="${j} ${mounthsAtribute[date.getMonth()+2]}"></span> <span class="day">${j}</span></div>`;
+        }
     }
     template.innerHTML = days;
 
     let cells = document.querySelectorAll('.calendar__cell');
-    for(let i = 0; i < week.length; i++) {
-        cells[i].insertAdjacentHTML("afterbegin",`<span class="week">${week[i]},</span>`)
-    }    
+    for (let i = 0; i < week.length; i++) {
+        cells[i].insertAdjacentHTML("afterbegin", `<span class="week">${week[i]},</span>`)
+    }
 }
 
-document.querySelector('#arrow-left').addEventListener('click',()=>{
+document.querySelector('#arrow-left').addEventListener('click', () => {
     date.setMonth(date.getMonth() - 1);
     renderCalendar();
 })
 
-document.querySelector('#arrow-right').addEventListener('click',()=>{
+document.querySelector('#arrow-right').addEventListener('click', () => {
     date.setMonth(date.getMonth() + 1);
     renderCalendar();
 })
 
 renderCalendar();
 
-document.querySelector('.today').addEventListener('click',()=>{
+document.querySelector('.today').addEventListener('click', () => {
     date.setMonth(new Date().getMonth());
     renderCalendar();
 })
-    
-
-    
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- logic.js
+++ logic.js
@@ -1,23 +1,26 @@
 const cells = document.querySelectorAll('.calendar__cell');
-
 const addPopup = document.querySelector('.add-popup');
 const addPopupCross = document.querySelector('.close-add');
-
 const quickPopupCross = document.querySelector('.close-quick');
 const quickPopup = document.querySelector('.quick-add');
 const addQuickPopup = document.getElementById('btn-add');
 const quickForm = document.getElementById('quick-form');
-
 const searchInput = document.querySelector('.control__search-input');
 const closeSearchInput = document.querySelector('.search-close');
-
 const eventSubmit = document.getElementById('event__submit');
 
 
 
-cells.forEach(el => el.addEventListener('click', () => addPopup.classList.remove('hidden')));
+cells.forEach(el => el.addEventListener('click', () => {
+    if (el.classList.contains('active')) {
+        addPopup.classList.remove('hidden');
+        addPopup.classList.add('change')
+    } else {
+        addPopup.classList.remove('hidden')
+    }
+}));
 
-function clearAddPopup () {
+function clearAddPopup() {
     addPopup.classList.add('hidden');
     document.querySelector('.event-input').value = '';
     document.querySelector('.date-input').value = '';
@@ -25,27 +28,41 @@ function clearAddPopup () {
     document.querySelector('.add__description').value = '';
 }
 
-addPopupCross.addEventListener('click',() => clearAddPopup());
+addPopupCross.addEventListener('click', () => clearAddPopup());
 
-quickPopupCross.addEventListener('click', function () {
-  quickPopup.classList.add('hidden');
-  document.querySelector('.quick-input').value = '';
-  addQuickPopup.classList.remove('active');
+quickPopupCross.addEventListener('click', function() {
+    quickPopup.classList.add('hidden');
+    document.querySelector('.quick-input').value = '';
+    addQuickPopup.classList.remove('active');
 });
 
 addQuickPopup.addEventListener('click', () => {
-  quickPopup.classList.remove('hidden');
-  addQuickPopup.classList.add('active')
+    quickPopup.classList.remove('hidden');
+    addQuickPopup.classList.add('active');
 })
 
-closeSearchInput.addEventListener('click', function () {
-  searchInput.value = '';
+closeSearchInput.addEventListener('click', function() {
+    searchInput.value = '';
 })
 
-eventSubmit.addEventListener('click', function () {
-  cells[0].insertAdjacentHTML("beforeend",`<h3 class="description-title"> ${document.querySelector('.event-input').value}</h3>`);
-  cells[0].insertAdjacentHTML("beforeend",`<div class="decription-persons"><p class="persons">${document.querySelector('.persons-input').value}</p></div>`);
-  cells[0].classList.add('active');
-  clearAddPopup ();
+eventSubmit.addEventListener('click', function() {
+    let titleValue = document.querySelector('.event-input').value;
+    let dateValue = document.querySelector('.date-input').value;
+    let personValue = document.querySelector('.persons-input').value;
+    let discriptionValue = document.querySelector('.add__description').value;
+    //localStorage.setItem('event-value', inputValue);
+    //let res = localStorage.getItem('event-value')
+    clearAddPopup();
+    addText(dateValue, titleValue, personValue, discriptionValue);
 
 })
+
+
+
+function addText(currentDay, currentTitle, currenPerson, currentDiscription) {
+    let cell = document.querySelector(`[day="${currentDay}"]`);
+    cell.insertAdjacentHTML('beforeend', `<span class="description__title">${currentTitle}</span>`);
+    cell.insertAdjacentHTML('beforeend', `<div class="description__persons"><p class="person">${currenPerson}</p></div>`);
+    cell.insertAdjacentHTML('beforeend', `<p class="description__content">${currentDiscription}</p>`);
+    cell.classList.add('active')
+}
